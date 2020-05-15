fc8ca1704af36a8b965e30d62f84d2ccd9b2447f
kohta ito <kohta110@gmail.com>
2020-05-15 23:24:31 +0900

    exponential backoff

--- src/modules/socket.types.ts
+++ src/modules/socket.types.ts
@@ -1,15 +1,23 @@
 export type SocketState = {
   socket: WebSocket
+  reconnectInterval: number
+  reconnectAttempts: number
+  reconnectTimer: number
 }
 
 export const SocketActions = {
-  Init: 'SocketAction:Init'
+  Init: 'SocketAction:Init',
+  Open: 'SocketAction:Open',
+  Close: 'SocketAction:Close'
 } as const
 
-export type SocketAction = {
-  type: typeof SocketActions.Init
-  payload: WebSocket
-}
+export type SocketAction =
+  | {
+      type: typeof SocketActions.Init
+      payload: WebSocket
+    }
+  | { type: typeof SocketActions.Open }
+  | { type: typeof SocketActions.Close; payload: { timer: number } }
 
 type Message = {
   id: string
